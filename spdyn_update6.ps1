995695936c009ab8e07ae92b82bfb2b43118c275
Atavar74 <114655433+Atavar74@users.noreply.github.com>
2024-12-14 14:04:43 +0100

    Create spdyn_update6.ps1
    Adapted for IPv6 standard
    simplification
    Accepts parameters (username/address, password)
    Template for automatic execution via Taskmanager

---
+++ spdyn_update6.ps1
@@ -0,0 +1,70 @@
+#requires -version 4
+# notepad++ %USERPROFILE%\.ssh\etc\spdyn_update6.ps1
+# powershell -NoProfile -ExecutionPolicy BYPASS -File "%USERPROFILE%\.ssh\etc\spdyn_update6.ps1" 'username' 'password'
+# schtasks /create /tn "dyndns"	 /sc ONSTART /ru "Administrator" /rp "admin" /tr "powershell -NoProfile -ExecutionPolicy Bypass -File 'C:\Users\Administrator\.ssh\etc\spdyn_update6.ps1'  'username' 'password'"
+# schtasks /create /tn "dyndnsH" /sc HOURLY  /ru "Administrator" /rp "admin" /tr "powershell -NoProfile -ExecutionPolicy Bypass -File 'C:\Users\Administrator\.ssh\etc\spdyn_update6.ps1'  'username' 'password'"
+
+Clear-Host
+
+# (mandatory) adjust your values
+$fqdn = $args[0]
+$pwd  = $args[1]
+$user = $args[0]
+
+# (optional) set full path to (writable) logfile and switch logging on ($true) or off ($false)
+$myLogFile = $PSScriptRoot + "\spdyn_update.log"
+$logging = $true
+
+### simple logger ###
+function log {
+	param(
+		[Parameter(ValueFromPipeline=$true)]
+		$piped
+	)
+
+	if ($logging) {
+		(Get-Date -Format "yyyy-MM-dd HH:mm:ss").ToString() + " " + `
+			$piped | Out-File -FilePath $myLogFile -Append
+		}
+	# console output
+	"$piped"
+}
+
+### Getting the IPv6 directly from the System, either use the fixed Address, or a randomized. (depending of what used for Firewall-opening) ###
+try {
+	#$currentIP = (Get-NetIPAddress -AddressFamily IPv6 -PrefixOrigin RouterAdvertisement -SuffixOrigin Link ).IPAddress
+	$currentIP = (Get-NetIPAddress -AddressFamily IPv6 -PrefixOrigin RouterAdvertisement -SuffixOrigin Random ).IPAddress
+	"Current IPv6: " + $currentIP | log
+} catch {
+	"Current IPv6 ERROR: " + $_.Exception.Message | log
+	exit 1
+}
+
+### Compare with registered Address, exit when same
+try {
+	"Resolve DNS Name " + "Using native Commandlet" | log
+		$ipHostEntry = Resolve-DnsName $fqdn -Type AAAA -ErrorAction Stop
+		$registeredIP = $ipHostEntry[0].IPAddress
+
+} catch {
+	"Resolve DNS Name " + $_.Exception.Message | log
+	exit 1
+}
+
+if ($registeredIP -like $currentIP) {
+	"Precheck " + "IP $currentIP already registered." | log
+	exit 0
+}
+
+### Send registration
+$secpasswd = ConvertTo-SecureString $pwd -AsPlainText -Force
+$myCreds = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+$url = "https://update.spdyn.de/nic/update?hostname=$fqdn&myip=$currentIP"
+
+try {
+	$resp = Invoke-WebRequest -Uri $url -Credential $myCreds -UseBasicParsing -ErrorAction Stop
+} catch {
+	"Update DNS " + $_.Exception.Message | log
+	exit 1
+}
+"SPDYN result " + $resp.Content | log
